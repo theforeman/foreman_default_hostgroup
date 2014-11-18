7ddf090a590baa5aa67e7a9dd986f0db3d3685a2
Nate Walck <n8@dropbox.com>
2014-11-18 09:14:08 -0800

    Acutally listening to the provided directions this time

--- lib/default_hostgroup_managed_host_patch.rb
+++ lib/default_hostgroup_managed_host_patch.rb
@@ -14,7 +14,7 @@ module DefaultHostgroupManagedHostPatch
 
       unless SETTINGS[:default_hostgroup] && SETTINGS[:default_hostgroup][:facts_map]
         Rails.logger.warn "DefaultHostgroupMatch: Could not load default_hostgroup map from settings, check config."
-        return host, result
+        return @host, result
       end
 
       Rails.logger.debug "DefaultHostgroupMatch: performing Hostgroup match"
@@ -22,36 +22,36 @@ module DefaultHostgroupManagedHostPatch
       if Setting[:force_hostgroup_match_only_new]
         # host.new_record? will only test for the early return in the core method, a real host
         # will have already been saved at least once.
-        unless host.present? && !host.new_record? && host.hostgroup.nil? && host.reports.empty?
+        unless @host.present? && !@host.new_record? && @host.hostgroup.nil? && @host.reports.empty?
 
           Rails.logger.debug "DefaultHostgroupMatch: skipping, host exists"
-          return host, result
+          return @host, result
         end
       end
 
       unless Setting[:force_hostgroup_match]
-        if host.hostgroup.present?
+        if @host.hostgroup.present?
           Rails.logger.debug "DefaultHostgroupMatch: skipping, host has hostgroup"
-          return host, result
+          return @host, result
         end
       end
 
       facts_map = SETTINGS[:default_hostgroup][:facts_map]
       new_hostgroup = find_match(facts_map)
 
-      return host, result unless new_hostgroup
+      return @host, result unless new_hostgroup
 
-      host.hostgroup = new_hostgroup
-      host.save(:validate => false)
+      @host.hostgroup = new_hostgroup
+      @host.save(:validate => false)
       Rails.logger.info "DefaultHostgroupMatch: #{hostname} added to #{new_hostgroup}"
 
-      return host, result
+      return @host, result
     end
 
     def group_matches?(fact)
       fact.each do |fact_name, fact_regex|
         fact_regex.gsub!(/(\A\/|\/\z)/, '')
-        host_fact_value = host.facts_hash[fact_name]
+        host_fact_value = @host.facts_hash[fact_name]
         Rails.logger.info "Fact = #{fact_name}"
         Rails.logger.info "Regex = #{fact_regex}"
         return true if Regexp.new(fact_regex).match(host_fact_value)
